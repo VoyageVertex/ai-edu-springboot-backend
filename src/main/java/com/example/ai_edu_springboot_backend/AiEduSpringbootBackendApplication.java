d9e21218ba86053af75760aacb3d7b0d346e06c4
Rajeev.Kumar <Rajeev.Kumar@PLRENT-PF3WGMLE.OpusConsulting.com>
2025-06-26 20:34:17 +0530

    Code changes for the spring boot

---
+++ src/main/java/com/example/ai_edu_springboot_backend/AiEduSpringbootBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ai_edu_springboot_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiEduSpringbootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiEduSpringbootBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ai_edu_springboot_backend/AiEduSpringbootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ai_edu_springboot_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiEduSpringbootBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
